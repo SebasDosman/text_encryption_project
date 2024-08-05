d00aff21ee3e6d36a6cecf7a0404258bc452073f
Juan Sebastián Dosman <jsdosman0@gmail.com>
2024-08-04 20:00:30 -0500

    feat: 🎸 first commit
    repository initialized

---
+++ README.md
@@ -0,0 +1,38 @@
+# Text Encryption App
+This project is a simple web application that encrypts and decrypts texts using a custom encryption scheme. The application allows users to exchange secret messages with others who know the encryption key.
+
+## Encryption Keys
+- The letter "e" is replaced with "enter"
+- The letter "i" is replaced with "imes"
+- The letter "a" is replaced with "ai"
+- The letter "o" is replaced with "ober"
+- The letter "u" is replaced with "ufat"
+
+## Requirements
+- The app should only work with lowercase letters.
+- No accented letters or special characters should be used.
+- It should be possible to convert a word to its encrypted version and vice versa.
+
+## Features
+- Input fields for entering text to be encrypted or decrypted.
+- Options to choose between encryption and decryption.
+- Display the result on the screen.
+- A button to copy the encrypted/decrypted text to the clipboard, similar to the ctrl+C command.
+
+## Technologies Used
+- HTML
+- CSS (Tailwind)
+- JavaScript
+
+## How to Use
+- Open the index.html file in your browser.
+- Enter the text you want to encrypt or decrypt.
+- Choose the appropriate option (Encrypt or Decrypt).
+- View the result on the screen.
+- Use the "Copy" button to copy the result to the clipboard.
+
+## Contributing
+Feel free to submit issues or pull requests if you find bugs or have suggestions for improvements.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+const encryptionKeys = {
+    e: 'enter',
+    i: 'imes',
+    a: 'ai',
+    o: 'ober',
+    u: 'ufat',
+};
+const decryptionKeys = Object.fromEntries(
+    Object.entries(encryptionKeys).map(([key, value]) => [value, key])
+);
+
+
+const getElementById = (id) => {
+    return document.getElementById(id);
+};
+
+const encryptText = (text) => {
+    return text.replace(/[eioua]/g, match => encryptionKeys[match]);
+};
+
+const decryptText = text => {
+    let result = text;
+
+    for (const [key, value] of Object.entries(decryptionKeys)) {
+        const regex = new RegExp(key, 'g');
+        result = result.replace(regex, value);
+    }
+
+    return result;
+};
+
+const validateInput = text => /^[a-z\s]+$/.test(text);
+
+const showBanner = message => {
+    const banner = getElementById('banner');
+    const bannerMessage = getElementById('banner-message');
+
+    bannerMessage.textContent = message;
+    banner.classList.remove('hidden');
+
+    setTimeout(() => banner.classList.add('hidden'), 3000);
+};
+
+const encryptByButton = () => {
+    const textInput = getElementById('text').value;
+    const resultTextarea = getElementById('text-result');
+
+    if (validateInput(textInput)) {
+        resultTextarea.value = encryptText(textInput);
+        getElementById('text').value = '';
+    } else {
+        showBanner('Please enter only lowercase letters without accents or special characters.');
+    }
+};
+
+const decryptByButton = () => {
+    const textInput = getElementById('text').value;
+    const resultTextarea = getElementById('text-result');
+
+    if (validateInput(textInput)) {
+        resultTextarea.value = decryptText(textInput);
+        getElementById('text').value = '';
+    } else {
+        showBanner('Please enter only lowercase letters without accents or special characters.');
+    }
+};
+
+const copyByButton = () => {
+    const resultTextarea = getElementById('text-result');
+
+    resultTextarea.select();
+    document.execCommand('copy');
+};
